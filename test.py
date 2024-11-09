a2806f78e0e5e2acd0bee8997b3c1c4d303e06f0
3LeafClov3r <127633281+3LeafClov3r@users.noreply.github.com>
2024-11-08 23:41:41 -0800

    iniyial

---
+++ test.py
@@ -0,0 +1,20 @@
+import pygame, sys, random
+ 
+pygame.init()
+ 
+WIDTH, HEIGHT = 1280, 720
+ 
+FONT = pygame.font.SysFont("Consolas", int(WIDTH/20))
+ 
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Pong!")
+CLOCK = pygame.time.Clock()
+ 
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+ 
+    pygame.display.update()
+    CLOCK.tick(300)
